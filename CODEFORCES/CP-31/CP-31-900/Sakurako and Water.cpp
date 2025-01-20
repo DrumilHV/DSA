99572858d283eba5932ad9b59a47b28a34c62b3c
drumil ved <drumilhved@gmail.com>
2025-01-20 21:29:32 +0530

    make index for each diagonal amazing tick

--- CODEFORCES/CP-31/CP-31-900/Sakurako and Water.cpp
+++ CODEFORCES/CP-31/CP-31-900/Sakurako and Water.cpp
@@ -12,46 +12,25 @@ void solve() {
     int n;
     cin >> n;
     int arr[n][n];
-    int neg = 0;
+    map<int, int> mp;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> arr[i][j];
-            if (arr[i][j] < 0) {
-                neg++;
-            }
+            mp[i - j] = min(mp[i - j], arr[i][j]);
         }
-    }
-    if (neg == 0) {
-        cout << 0 << endl;
-        return;
     }
     int ans = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            int size = min(n - i, n - j);
-            int mn = 0;
-            for (int k = i; k < i + size; k++) {
-                for (int l = j; l < j + size; j++) {
-                    if (arr[k][k] < 0 && l == k) {
-                        mn = min(mn, arr[k][k]);
-                    }
-                }
-            }
-            cout << endl;
-            for (int k = i; k < i + size; k++) {
-                arr[k][k] += abs(mn);
-            }
-            ans += abs(mn);
-        }
+    for (auto i : mp) {
+        ans += abs(i.second);
     }
-    cout << endl << ans << endl << endl;
+    cout << ans << endl;
 }
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
     int t;
     cin >> t;
     while (t--) {
